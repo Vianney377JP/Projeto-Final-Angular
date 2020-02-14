09df0af8890dfd0700a49641606b094688bf3766
Vianney Fernandes <52356994+Vianney377JP@users.noreply.github.com>
2020-02-14 16:39:11 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'pacientes',
+    // children: [
+    //   {component, path, children: []}
+    // ],
+    loadChildren: () => import('./paciente/paciente.module')
+      .then(mod => mod.PacienteModule),
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MedicoModule } from './medico/medico.module';
+import { PacienteModule } from './paciente/paciente.module';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MedicoModule,
+    PacienteModule,
+    CommonModule
+        
+  ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
